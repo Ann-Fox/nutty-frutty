a1b7f6058ddb105e898c9fa83bbc7f8a16a6bf2f
Ann-Fox <fx8565@mail.ru>
2023-09-21 13:28:37 +0500

    add Assortiment

--- src/components/AssortmentComponent.vue
+++ src/components/AssortmentComponent.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="center">
-        <h1>Наш ассортимент</h1>
+    <div class="assortiment center">
+        <h2>Наш ассортимент</h2>
         <div class="cards">
             <AssortmentItem></AssortmentItem>
         </div>
@@ -13,5 +13,14 @@ import AssortmentItem from './AssortmentItem.vue';
 </script>
 
 <style lang="scss" scoped>
-
+.assortiment {
+    margin: 178px auto 90px;
+}
+.cards {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    grid-column-gap: 26px;
+    grid-row-gap: 54px;
+}
 </style>
--- src/components/AssortmentItem.vue
+++ src/components/AssortmentItem.vue
@@ -1,22 +1,27 @@
 <template>
-    <div class="card">
+    <div class="card bg" v-for="product in products" :key="product.id">
         <div class="card__img">
-            <img src="" alt="Абрикосы сушеные">
+            <img :src="product.imgUrl" :alt="product.name" />
         </div>
         <div class="card__info">
-            <h4 class="card__info__head">Абрикосы сушеные</h4>
-            <p class="card__info__description">Свежие Сушенные абрикосы в герметичной упаковке от фирмы</p>
+            <h4 class="card__info__head">{{ product.name }}</h4>
+            <p class="card__info__description">Свежие {{ product.description }}<br>
+                в герметичной упаковке от фирмы</p>
             <div class="card__info__price">
-                <p class="card__info__price__new">От 322.00 P</p>
-                <p class="card__info__price__old">От 450.00 Р</p>
+                <p class="card__info__price__new">От {{ product.priceNew }} P</p>
+                <p class="card__info__price__old">От {{ product.priceOld }} Р</p>
             </div>
             <div class="card__info__btns">
-                    <button>
-                        <IconArrows></IconArrows>
-                    </button>
-                    <button></button>
-                    <button></button>
-                    <button>В корзину</button>
+                <button>
+                    <IconArrows></IconArrows>
+                </button>
+                <button>
+                    <IconSearchSmall></IconSearchSmall>
+                </button>
+                <button>
+                    <IconFaivorites></IconFaivorites>
+                </button>
+                <button>В корзину</button>
             </div>
         </div>
     </div>
@@ -24,9 +29,160 @@
 
 <script setup>
 import IconArrows from '../components/icons/IconArrows.vue'
-  
+import IconSearchSmall from '../components/icons/IconSearchSmall.vue'
+import IconFaivorites from '../components/icons/IconFaivorites.vue'
+
+const products = [
+    {
+        id: 1,
+        imgUrl: '../public/images/assortiment/apricot.png',
+        name: 'Абрикосы сушеные',
+        description: 'Сушенные абрикосы',
+        priceNew: 322.00,
+        priceOld: 450.00,
+    },
+    {
+        id: 1,
+        imgUrl: '../public/images/assortiment/apricot.png',
+        name: 'Абрикосы сушеные',
+        description: 'Сушенные абрикосы',
+        priceNew: 322.00,
+        priceOld: 450.00,
+    },
+    {
+        id: 1,
+        imgUrl: '../public/images/assortiment/apricot.png',
+        name: 'Абрикосы сушеные',
+        description: 'Сушенные абрикосы',
+        priceNew: 322.00,
+        priceOld: 450.00,
+    },
+    {
+        id: 1,
+        imgUrl: '../public/images/assortiment/apricot.png',
+        name: 'Абрикосы сушеные',
+        description: 'Сушенные абрикосы',
+        priceNew: 322.00,
+        priceOld: 450.00,
+    },
+    {
+        id: 1,
+        imgUrl: '../public/images/assortiment/apricot.png',
+        name: 'Абрикосы сушеные',
+        description: 'Сушенные абрикосы',
+        priceNew: 322.00,
+        priceOld: 450.00,
+    },
+    {
+        id: 1,
+        imgUrl: '../public/images/assortiment/apricot.png',
+        name: 'Абрикосы сушеные',
+        description: 'Сушенные абрикосы',
+        priceNew: 322.00,
+        priceOld: 450.00,
+    },
+    {
+        id: 1,
+        imgUrl: '../public/images/assortiment/apricot.png',
+        name: 'Абрикосы сушеные',
+        description: 'Сушенные абрикосы',
+        priceNew: 322.00,
+        priceOld: 450.00,
+    },
+    {
+        id: 1,
+        imgUrl: '../public/images/assortiment/apricot.png',
+        name: 'Абрикосы сушеные',
+        description: 'Сушенные абрикосы',
+        priceNew: 322.00,
+        priceOld: 450.00,
+    },
+]
 </script>
 
 <style lang="scss" scoped>
+.card {
+    border-radius: 26.736px;
+    padding: 15px 15px 30px;
 
+    &__img {
+        margin-bottom: 23px;
+
+    }
+
+    &__info {
+        &__head {
+            color: #000;
+            font-size: 21.389px;
+            font-style: normal;
+            font-weight: 700;
+            line-height: normal;
+            letter-spacing: 0.891px;
+            margin-bottom: 13px;
+        }
+
+        &__description {
+            color: #363636;
+            font-size: 16.041px;
+            line-height: 23.171px;
+            /* 144.444% */
+            letter-spacing: 0.891px;
+            margin-bottom: 18px;
+            max-width: 291px;
+        }
+
+        &__price {
+            // font-family: Roboto;
+            font-size: 16.041px;
+            font-style: normal;
+            line-height: 23.171px;
+            /* 144.444% */
+            letter-spacing: 0.891px;
+            margin-bottom: 31px;
+            display: flex;
+            gap: 21px;
+
+            &__new {
+                color: #DA5C5C;
+                font-weight: 700;
+
+            }
+
+            &__old {
+                color: rgba(54, 54, 54, 0.25);
+                font-weight: 700;
+                text-decoration: line-through;
+
+            }
+        }
+
+        &__btns {
+            align-items: center;
+            display: flex;
+            justify-content: space-between;
+
+        & button {
+            border-radius: 13.368px;
+            // background-color: #F8F8F8;
+            background-color: #e6eaf3;
+            width: 55.254px;
+            height: 55.254px;
+
+            &:last-child {
+                background-color: #FEB302;
+                width: 184.477px;
+                height: 55.254px;
+
+                color: #FFF;
+                font-size: 21.389px;
+                font-style: normal;
+                font-weight: 700;
+                line-height: normal;
+                letter-spacing: 0.891px;
+            }
+        }
+    }
+
+}
+}
 </style>
