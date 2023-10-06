10da87e8fa010c4b3ad2c57655b95ed937a3d4a1
Ann-Fox <fx8565@mail.ru>
2023-10-06 17:38:16 +0500

    add contacts

---
+++ src/views/ContactsView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about center">
+    <h2>Контакты</h2>
+    <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн.
+    </p>
+     <LogoMainBig/>
+  </div>
+ 
+</template>
+
+<script setup>
+import LogoMainBig from '../components/icons/LogoMainBig.vue';
+</script>
+
+<style lang="scss" scoped>
+.about {
+  margin: 42px 144px 143px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 1632px;
+  gap: 35px;
+}
+
+</style>
