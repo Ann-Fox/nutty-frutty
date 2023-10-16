2f33272f0b1041f66d41fef59b7bafd46297e59d
Ann-Fox <fx8565@mail.ru>
2023-10-16 14:02:18 +0500

    add RouterLink

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,35 +1,47 @@
 <template>
     <div class="background">
         <header class="header center">
-        <div class="header__left">
-            <div class="header__left__logo">
-                <RouterLink to="/"><LogoMain/></RouterLink>
+            <div class="header__left">
+                <div class="header__left__logo">
+                    <RouterLink to="/">
+                        <LogoMain />
+                    </RouterLink>
+                </div>
+                <nav class="header__left__nav">
+                    <ul>
+                        <li>
+                            <RouterLink to="/">Главная</RouterLink>
+                        </li>
+                        <li>Каталог</li>
+                        <li>
+                            <RouterLink to="/about">О нас</RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink to="/about-delivery">О доставке</RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink to="/our-stores">Магазины</RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink to="/contacts">Контакты</RouterLink>
+                        </li>
+                    </ul>
+                </nav>
             </div>
-            <nav class="header__left__nav">
-                <ul>
-                    <li><RouterLink to="/">Главная</RouterLink></li>
-                    <li>Каталог</li>
-                    <li><RouterLink to="/about">О нас</RouterLink></li>
-                    <li><RouterLink to="/about-delivery">О доставке</RouterLink></li>
-                    <li><RouterLink to="/our-stores">Магазины</RouterLink></li>
-                    <li><RouterLink to="/contacts">Контакты</RouterLink></li>
-                </ul>
-            </nav>
-        </div>
-        <div class="header__rigth">
-            <div class="header__rigth__phone">
-                <IconPhone/>
-                <p>+7(912) 123 85-65</p>
-            </div>
-            <div class="header__rigth__favorites">Избранное</div>
-            <div class="header__rigth__basket">
-                <p class="header__rigth__basket__text">Корзина</p>
-                <div class="header__rigth__basket__count">
-                    <p>3</p>
+            <div class="header__rigth">
+                <div class="header__rigth__phone">
+                    <IconPhone />
+                    <p>+7(912) 123 85-65</p>
+                </div>
+                <div class="header__rigth__favorites">Избранное</div>
+                <div class="header__rigth__basket">
+                    <RouterLink to="/basket" class="header__rigth__basket__text">Корзина</RouterLink>
+                    <div class="header__rigth__basket__count">
+                        <p>3</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    </header>
+        </header>
     </div>
 </template>
 
@@ -38,7 +50,7 @@
 import LogoMain from './icons/LogoMain.vue';
 import IconPhone from './icons/IconPhone.vue'
 import { RouterLink } from 'vue-router';
-    export default {
+export default {
     components: { LogoMain, IconPhone, RouterLink }
 }
 </script>
@@ -49,7 +61,7 @@ import { RouterLink } from 'vue-router';
 //     box-shadow: 0px 2px 15px 0px rgba(0, 0, 0, 0.10);
 //  }
 
- .header {
+.header {
     display: flex;
     padding-top: 61px;
     padding-bottom: 47px;
@@ -106,14 +118,15 @@ import { RouterLink } from 'vue-router';
             justify-content: space-around;
             align-items: center;
 
-            &__text { 
+            &__text {
                 color: #FFF;
                 font-size: 18px;
                 font-style: normal;
                 font-weight: 700;
                 line-height: normal;
-               
-                        }
+
+            }
+
             &__count {
                 width: 38px;
                 height: 39px;
@@ -133,5 +146,4 @@ import { RouterLink } from 'vue-router';
             }
         }
     }
- }
-</style>
+}</style>
